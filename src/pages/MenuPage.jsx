58c1a1157b772da10cea0c9857eef2e94fac5a34
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-12-15 23:33:40 +0200

    Updated routing at MenuPage

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,14 +1,30 @@
 import { Button } from '../components/UI/Button/Button';
+import {useNavigate} from "react-router";
+import {useLocalStorage} from "../hooks/useLocalStorage.jsx";
+import {BUTTON_STATES} from "../constants.js";
 
-export function MenuPage(props) {
-    const { onStart, onSettings } = props;
+export function MenuPage() {
+    const navigate = useNavigate();
+
+    const [appSettings] = useLocalStorage('battleship-settings', {
+        playerName: 'Player'
+    });
+
+    const handleStart = () => {
+        navigate(`/game/${appSettings.playerName}`);
+    };
+
+    const handleSettings = () => {
+        navigate('/settings');
+    };
 
     return (
         <div>
             <h1>Морський Бій</h1>
             <p>Знищи кораблі супротивника на полі 5x5</p>
-            <Button onClick={onStart}>Почати гру</Button>
-            <Button onClick={onSettings}>Налаштування</Button>
+            <p>Гравець {appSettings.playerName}</p>
+            <Button onClick={handleStart} variant={BUTTON_STATES.PRIMARY}>Почати гру</Button>
+            <Button onClick={handleSettings} variant={BUTTON_STATES.SECONDARY}>Налаштування</Button>
         </div>
     );
 }
